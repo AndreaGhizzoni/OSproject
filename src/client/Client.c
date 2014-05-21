3264d9e7128c8da0f315309189f56d4e85afc34e
FedeLago <fede.a@live.it>
2014-05-21 15:34:20 +0200

    edited makefileand first implementation of client

---
+++ src/client/Client.c
@@ -0,0 +1,22 @@
+/********************************
+*								*
+*			AUTHORS				*
+*								*
+*********************************
+Andrea Ghizzoni 157507
+Federica Lago 157955
+PROJECT: #1 CODEC
+COURSE: Sistemi Operativi
+YEAR: 2014
+*/
+
+
+//conto numero argomenti
+//controllo quale server
+void leggi()
+
+
+int main(int argc, char** argv) {
+
+	return 0;
+}
---
+++ src/client/include/client_args.h
@@ -0,0 +1,76 @@
+/********************************
+*								*
+*			AUTHORS				*
+*								*
+*********************************
+Andrea Ghizzoni 157507
+Federica Lago 157955
+PROJECT: #1 CODEC
+COURSE: Sistemi Operativi
+YEAR: 2014
+*/
+
+#ifndef CLIENT_ARGS
+#define CLIENT_ARGS
+#endif
+
+#include <ctype.c>
+
+#define OUTPUT_FILE "_output.txt"
+
+typedef struct{
+    char* nameServer; 	//MANDATORY
+    char* key;			//MANDATORY
+    int isFile;
+    char* fileName;
+    char* message;
+    char* output;
+    int encode;			//MANDATORY
+} Client_args;
+
+/********************************
+*								*
+*			SETTERS				*
+*								*
+********************************/
+
+void setName(Client_args* c, char* value) {
+	c.name=value;
+}
+
+void setKey(Client_args* c, char* value) {
+	int i=0;
+	while ((*value)[i]) {
+		if (!isalpha((*value)[i]))
+			return;
+		i++;
+	}
+	c.name=value;
+}
+
+void setisFile(Client_args* c, int isFile) {
+	c.isFile=isFile;
+}
+
+void setFileName(Client_args* c, char* value) {
+	c.name=value;
+}
+
+void setMessage(Client_args* c, char* value) {
+	c.message=value;
+}
+
+void setOutput(Client_args* c, char* value) {
+	if (value==NULL) {
+		c.output=OUTPUT_FILE;
+	}
+	else {
+		//controllo se è un nome di file valido
+		c.output=value;
+	}
+}
+
+int setEncode (Client_args* c, int value) {
+	c.encode=value;
+	return 0;
+}
