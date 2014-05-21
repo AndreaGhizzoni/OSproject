7b0efcf3c77dd3fb598dd271ddf1dc12f01b745a
AndreaGhizzoni <andrea.ghz@gmail.com>
2014-05-21 10:39:47 +0200

    first commit client/server

---
+++ src/server/server.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "server_args.h"
+
+int main(int argc, char** argv){
+
+    return 0;
+}
+
---
+++ src/server/server_args.h
@@ -0,0 +1,14 @@
+#ifndef SERVER_ARGS
+#define SERVER_ARGS
+
+typedef struct{
+    char* name;
+    int msgmax;
+    int keymin;
+    int keymax;
+} Server_args;
+
+//funzione che inizializza il server
+
+//
+#endif
