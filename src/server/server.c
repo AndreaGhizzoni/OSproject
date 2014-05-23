846182e9ab0029c5b3f31f244ff35256a6b78591
AndreaGhizzoni <andrea.ghz@gmail.com>
2014-05-23 09:04:47 +0200

    fixed make file

--- src/server/server.c
+++ src/server/server.c
@@ -2,20 +2,25 @@
 #include <stdlib.h>
 #include "include/server_args.h"
 
-void parse_args(int, char**);
+Server_args* parse_args(int, char**);
 
 int main(int argc, char** argv){
-    parse_args(argc, argv);
+    Server_args* s;
+    s = parse_args(argc, argv);
+    if(s!=NULL)
+        print(s);
+    
 
     return 0;
 }
 
-void parse_args(int argc, char** argv){
+Server_args* parse_args(int argc, char** argv){
     int err;
     Server_args* sargs = alloc();
     err = populate(sargs, argc, argv);
     if(err==0)
-        print(sargs);
-    else
-        printf("%d\n",err);
+        return sargs;
+    
+    printf("%d\n",err);
+    return NULL;
 }
